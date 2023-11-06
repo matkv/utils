9f84906af238b4b082dc1618688747c12e0dca2f
matkv <mail@matkv.dev>
2023-11-06 18:28:01 +0100

    show logged in user when running utils

--- main.go
+++ main.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/user"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
+
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	app := &cli.App{
 		Name:  "utils",
-		Usage: "this is the utils cli app",
+		Usage: "automating some personal tasks",
 		Action: func(*cli.Context) error {
-			fmt.Println("Hello world!")
+			fmt.Printf("Hello %s! Please choose the action:", user.Username)
 			return nil
 		},
 	}
