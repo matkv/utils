4f77a6957ffe814c651910f212a97b6ae568864c
matkv <mail@matkv.dev>
2024-03-13 20:03:19 +0100

    played around with Copilot a bit

--- main.go
+++ main.go
@@ -1,58 +1,48 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
 	"os/user"
-
-	"github.com/urfave/cli/v2"
 )
 
+type Action interface {
+	GetName() string
+}
+
+type CreateAction struct{}
+type DeleteAction struct{}
+type ListAction struct{}
+
+func (a *CreateAction) GetName() string {
+	return "Create"
+}
+
+func (a *DeleteAction) GetName() string {
+	return "Delete"
+}
+
+func (a *ListAction) GetName() string {
+	return "List"
+}
+
 func main() {
+	user := getCurrentUser()
+	fmt.Println("Current user:", user.Username)
 
-	user, err := user.Current()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	fmt.Println("Please select one of the following options:")
 
-	reader := bufio.NewReader(os.Stdin)
-
-	app := &cli.App{
-		Name:  "utils",
-		Usage: "automating some personal tasks",
-		Action: func(context *cli.Context) error {
-			args := context.Args()
-			if args.Len() > 0 {
-				fmt.Printf("Hello, your argument was: %q", args.Get(0))
-			} else {
-				printPossibleActions(user.Username)
-				chosenAction := readInput(reader)
-				fmt.Printf("You chose: %q", chosenAction)
-			}
-
-			return nil
-		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+	// let the user select one out of several actions that are represented by a Action type
+	actions := []Action{&CreateAction{}, &DeleteAction{}, &ListAction{}}
+	for i, action := range actions {
+		fmt.Printf("%d: %s\n", i, action.GetName())
 	}
 }
 
-func printPossibleActions(username string) {
-	fmt.Printf("Hello %s! Please choose the action:\n\n", username)
-	fmt.Printf("1. Create changelog for hugo website\n")
-	fmt.Printf("2. Create micro blog post for hugo website")
-}
-
-func readInput(reader *bufio.Reader) string {
-
-	option, err := reader.ReadString('\n')
+func getCurrentUser() *user.User {
+	currentUser, err := user.Current()
 	if err != nil {
-		log.Fatal((err))
+		fmt.Println("Error:", err)
+		return nil
 	}
-
-	return option
+	return currentUser
 }
