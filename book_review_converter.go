ea172732e828afc7046c0c17061133ff83218a4b
matkv <mail@matkv.dev>
2024-09-19 20:50:46 +0200

    use directory name as parameter

--- book_review_converter.go
+++ book_review_converter.go
@@ -8,6 +8,17 @@ import (
 )
 
 func UpdateBookreviews(directory string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get current directory: %v", err)
+	}
+
+	dirPath := filepath.Join(cwd, directory)
+
+	// Check if the directory exists
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		return fmt.Errorf("directory does not exist: %s", dirPath)
+	}
 
 	return filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
