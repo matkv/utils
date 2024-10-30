aa078a3a9810fb0edc17037ff0931b9dbdd66e5b
matkv <mail@matkv.dev>
2024-10-30 20:09:06 +0100

    use switch case, remove unneeded function + exit when config fails to load

--- cmd/utils/main.go
+++ cmd/utils/main.go
@@ -16,43 +16,32 @@ func main() {
 	dotfilestools.Config = config
 
 	if len(os.Args) > 1 {
-		if os.Args[1] == "pull" {
+		switch os.Args[1] {
+		case "pull":
 			dotfilestools.PullLatesDotfiles()
-			return
-		}
-
-		if os.Args[1] == "sync" {
+		case "sync":
 			dotfilestools.MoveConfigFiles()
-			return
-		}
-
-		if os.Args[1] == "bookreviews" {
+		case "bookreviews":
 			if len(os.Args) < 3 {
 				fmt.Println("Please provide a directory")
 				return
 			}
-
 			err := hugoTools.UpdateBookreviews(os.Args[2])
 			if err != nil {
 				fmt.Printf("Error processing directory: %v\n", err)
 			}
+		default:
+			fmt.Println("Unknown command")
 		}
+		return
 	}
-
-	readUserInput() // TEMP
-}
-
-func readUserInput() {
-	var input string
-	fmt.Print("Enter some input: ")
-	fmt.Scanln(&input)
-	fmt.Println("You entered:", input)
 }
 
 func loadConfig() *config.Config {
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 
 	// TODO these directories are wrong when running in debug mode in VSCode
