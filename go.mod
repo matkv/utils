c5498868a913090095290ec66bbe4db6cab6ace8
matkv <mail@matkv.dev>
2024-11-04 19:40:52 +0100

    add basic styling

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.22.1
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/bubbletea v1.1.2 // indirect
-	github.com/charmbracelet/lipgloss v0.13.0 // indirect
-	github.com/charmbracelet/x/ansi v0.4.0 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0 // indirect
+	github.com/charmbracelet/x/ansi v0.4.2 // indirect
 	github.com/charmbracelet/x/term v0.2.0 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
--- internal/ui/main.go
+++ internal/ui/main.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	titleStyle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("205")).
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("62")). // A darker color for the border
+			Padding(1, 2)
+
+	countStyle = lipgloss.NewStyle().
+			Padding(0, 1).
+			Margin(1, 0).
+			Background(lipgloss.Color("63")).
+			Foreground(lipgloss.Color("230")).
+			Border(lipgloss.ThickBorder()).
+			BorderForeground(lipgloss.Color("63"))
 )
 
 type Model struct {
@@ -30,5 +48,13 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	return fmt.Sprintf("Count: %d\n\nPress up/down to increase/decrease. Press q to quit", m.count)
+	title := titleStyle.Render("Counter")
+	count := countStyle.Render(fmt.Sprintf("Count: %d", m.count))
+
+	return fmt.Sprintf(
+		"%s\n\n%s\n\n%s",
+		title,
+		count,
+		"Press up/down to increase/decrease. Press q to quit.",
+	)
 }
