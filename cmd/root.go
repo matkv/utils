86e615afb5a9149cf1db92807b65f889a87e3464
matkv <mail@matkv.dev>
2024-11-06 21:34:02 +0100

    basic setup of viper to read config file

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
@@ -14,12 +15,44 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 		fmt.Println("Hello from Cobra!")
+		fmt.Println("Config path:", viper.ConfigFileUsed())
+		fmt.Println("Dotfiles path:", viper.GetString("dotfiles.path"))
+
+		cmd.Help()
+		if err := cmd.Help(); err != nil {
+			fmt.Println("Error displaying help:", err)
+			os.Exit(1)
+		}
 	},
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Failed to get user home directory:", err)
+		os.Exit(1)
+	}
+
+	viper.SetConfigName(".utils")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(home)
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Failed to read config file:", err)
 		os.Exit(1)
 	}
 }
