14176f0f7ea64b33cfc15c5b931752cecf2fd081
matkv <mail@matkv.dev>
2024-11-06 20:25:50 +0100

    remove unneeded code before setting up cobra

--- cmd/utils/main.go
+++ cmd/utils/main.go
@@ -1,49 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
 	config "github.com/matkv/utils/internal"
 	dotfilestools "github.com/matkv/utils/internal/dotfiles-tools"
 	hugotools "github.com/matkv/utils/internal/hugo-tools"
-	"github.com/matkv/utils/internal/ui"
 	workouttracker "github.com/matkv/utils/internal/workout-tracker"
 )
 
 func main() {
-
-	p := tea.NewProgram(ui.NewModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting program: %v", err)
-		os.Exit(1)
-	}
-
-	return // TEMP
-	printHellos()
-
-	// workouttracker.GenerateWorkoutGraph()
-	hugotools.CreateMovieReviews()
-
-	return // temp
-	config := loadConfig()
-	dotfilestools.Config = config
-}
-
-func loadConfig() *config.Config {
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
-
-	// TODO these directories are wrong when running in debug mode in VSCode
-
-	fmt.Println("Home:", cfg.Home)
-	fmt.Println("Dotfiles path:", cfg.DotfilesPath)
-
-	return cfg
 }
 
 func printHellos() {
