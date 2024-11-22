500df51210d193ef14172ebfd5b0de19608116c0
matkv <mail@matkv.dev>
2024-11-22 21:33:01 +0100

    create basic journal command

---
+++ cmd/journal.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+
+	journal "github.com/matkv/utils/internal/journal"
+	"github.com/spf13/cobra"
+)
+
+// journalCmd represents the journal command
+var journalCmd = &cobra.Command{
+	Use:   "journal",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files	
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("journal command called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(journalCmd)
+	journal.Hello()
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// journalCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// journalCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ internal/journal/obsidian_journal.go
@@ -0,0 +1,7 @@
+package journal
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello from journal package.")
+}
