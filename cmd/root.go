ac62ae09c4534ffa98f9ef742b9b57d7c9b0a8f8
matkv <mail@matkv.dev>
2024-11-22 22:50:14 +0100

    move config logic to separate file + create default config if none exists

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,12 @@
 package cmd
 
 import (
+	_ "embed"
 	"fmt"
 	"os"
-	"path/filepath"
 
+	"github.com/matkv/utils/internal/config"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
@@ -14,9 +14,7 @@ var rootCmd = &cobra.Command{
 	Short: "CLI tool to automate some personal tasks",
 	Long:  `CLI tool to automate some personal tasks. Some of the tasks include managing dotfiles, managing my hugo website and updating my workout tracker.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello from Cobra!")
-		fmt.Println("Config path:", viper.ConfigFileUsed())
-		fmt.Println("Dotfiles path:", viper.GetString("dotfiles.path"))
+		fmt.Println("root command called")
 	},
 }
 
@@ -28,53 +26,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-}
-
-func initConfig() {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		fmt.Println("Failed to get user home directory:", err)
-		os.Exit(1)
-	}
-
-	configPath := filepath.Join(home, ".config", "utils")
-	viper.AddConfigPath(configPath)
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			createConfigFile(configPath)
-		} else {
-			fmt.Printf("Failed to read config file: %v\n", err)
-			os.Exit(1)
-		}
-	}
-
-	configType := viper.GetString("configType")
-	settings := viper.Sub(configType)
-	if settings == nil {
-		fmt.Println("No configuration found for type:", configType)
-		os.Exit(1)
-	}
-
-	viper.MergeConfigMap(settings.AllSettings())
-}
-
-func createConfigFile(configPath string) {
-	configFilePath := filepath.Join(configPath, "config.yaml")
-	if err := os.MkdirAll(filepath.Dir(configFilePath), os.ModePerm); err != nil {
-		fmt.Println("Failed to create config directory:", err)
-		os.Exit(1)
-	}
-	file, err := os.Create(configFilePath)
-	if err != nil {
-		fmt.Println("Failed to create config file:", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-	fmt.Println("Created new config file at:", configFilePath)
+	fmt.Println("Initializing configuration...")
+	cobra.OnInitialize(config.InitConfig)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,74 @@
+package config
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+//go:embed default.yaml
+var defaultConfig []byte
+
+func InitConfig() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Failed to get user home directory:", err)
+		os.Exit(1)
+	}
+
+	configPath := filepath.Join(home, ".config", "utils")
+	viper.AddConfigPath(configPath)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			createConfigFile(configPath)
+		} else {
+			fmt.Printf("Failed to read config file: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	configType := viper.GetString("configType")
+	settings := viper.Sub(configType)
+	if settings == nil {
+		fmt.Println("No configuration found for type:", configType)
+		os.Exit(1)
+	}
+
+	viper.MergeConfigMap(settings.AllSettings())
+	PrintSettings()
+}
+
+func createConfigFile(configPath string) {
+	configFilePath := filepath.Join(configPath, "config.yaml")
+	if err := os.MkdirAll(filepath.Dir(configFilePath), os.ModePerm); err != nil {
+		fmt.Println("Failed to create config directory:", err)
+		os.Exit(1)
+	}
+	file, err := os.Create(configFilePath)
+	if err != nil {
+		fmt.Println("Failed to create config file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	fmt.Println("Created new config file at:", configFilePath)
+
+	if _, err := file.Write(defaultConfig); err != nil {
+		fmt.Println("Failed to write default config to file:", err)
+		os.Exit(1)
+	}
+}
+
+func PrintSettings() {
+	fmt.Println("Config path:", viper.ConfigFileUsed())
+	fmt.Println("Dotfiles path:", viper.GetString("dotfiles.path"))
+
+	fmt.Println("Config built.")
+}
