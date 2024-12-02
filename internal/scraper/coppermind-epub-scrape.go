f2647b981c889deef9a8b32f34d3b035d285ba9f
matkv <mail@matkv.dev>
2024-12-02 20:07:48 +0100

    WIP coppermind scraper

--- internal/scraper/coppermind-epub-scrape.go
+++ internal/scraper/coppermind-epub-scrape.go
@@ -1,7 +1,53 @@
 package scraper
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func Hello() {
 	fmt.Println("Hello from scraper package.")
+}
+
+func ScrapeSummaries() {
+	fmt.Println("Scraping summaries from Coppermind.")
+
+	// summaries links
+	links := []string{
+		"https://coppermind.net/wiki/Summary:The_Way_of_Kings",
+		"https://coppermind.net/wiki/Summary:Words_of_Radiance",
+		"https://coppermind.net/wiki/Summary:Oathbringer",
+		"https://coppermind.net/wiki/Summary:Rhythm_of_War",
+		"https://coppermind.net/wiki/Summary:Dawnshard",
+		"https://coppermind.net/wiki/Summary:Edgedancer"}
+
+	for _, link := range links {
+		fmt.Println("Scraping link:", link)
+		ScrapeSummary(link)
+		return // for now, only scrape the first link
+	}
+}
+
+func ScrapeSummary(link string) {
+	dir := "/home/matko/code/utils/internal/scraper/html"
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println("Error reading directory:", err)
+		return
+	}
+
+	for _, file := range files {
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".html") {
+			continue
+		}
+		filePath := dir + "/" + file.Name()
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Println("Error reading file:", filePath, err)
+			continue
+		}
+		fmt.Println("Reading file:", file.Name())
+		fmt.Println(string(content))
+	}
 }
