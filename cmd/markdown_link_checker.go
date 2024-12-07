499f5aadba135a167cbd19cc95d30ea7ad9bee23
matkv <mail@matkv.dev>
2024-12-07 21:37:36 +0100

    started working on markdown links checker

--- cmd/markdown_link_checker.go
+++ cmd/markdown_link_checker.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"fmt"
 
+	hugotools "github.com/matkv/utils/internal/hugo-tools"
 	"github.com/spf13/cobra"
 )
 
@@ -21,19 +22,11 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("markdown-link-checker called")
+		hugotools.Check("test")
 	},
 }
 
 func init() {
 	hugoCmd.AddCommand(markdownLinkCheckerCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// markdownLinkCheckerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// markdownLinkCheckerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ internal/hugo-tools/markdown_check.go
@@ -0,0 +1,9 @@
+package hugotools
+
+import "fmt"
+
+func Check(path string) error {
+	fmt.Println("Checking markdown file:", path)
+
+	return nil
+}
