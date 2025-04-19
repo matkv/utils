2eac1f042170ae9fcaf4963357d67cc7855d42e7
matkv <mail@matkv.dev>
2025-04-19 12:29:37 +0200

    move linux journal entry logic to separate function

--- internal/journal/obsidian_journal.go
+++ internal/journal/obsidian_journal.go
@@ -31,53 +31,64 @@ func openEditor() {
 	fmt.Println("Operating system:", operatingSystem)
 
 	if operatingSystem == "linux" {
-		fmt.Println("Opening Neovim editor.")
-
-		cmd := exec.Command("nvim")
-		cmd.Dir = config.ViperConfig.GetString("obsidian.vaultpath")
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		// Create a temporary file
-		tmpFile, err := os.CreateTemp("", "journal_*.md")
-		if err != nil {
-			fmt.Println("Error creating temporary file:", err)
-			return
-		}
-		defer os.Remove(tmpFile.Name())             // Clean up the temporary file after use
-		cmd.Args = append(cmd.Args, tmpFile.Name()) // Pass the temporary file to Neovim
-		fmt.Println("Temporary file created:", tmpFile.Name())
-		// Open Neovim with the temporary file
-		err = cmd.Start()
-		if err != nil {
-			fmt.Println("Error starting Neovim:", err)
-			return
-		}
-		fmt.Println("Neovim editor opened. Please write your journal entry.")
-
-		err = cmd.Wait()
-		if err != nil {
-			fmt.Println("Error waiting for Neovim to close:", err)
-			return
-		}
-
-		// Read the content of the temporary file
-		content, err := os.ReadFile(tmpFile.Name())
-		if err != nil {
-			fmt.Println("Error reading temporary file:", err)
-			return
-		}
-
-		fmt.Println("Journal entry content:")
-		fmt.Println(string(content))
-
-		fmt.Println("Neovim editor closed.")
-
+		createJournalEntryLinux()
 	}
 	if operatingSystem == "windows" {
 		fmt.Println("Opening Windows editor.")
 		// Open Windows editor here
 	}
 
+}
+
+func createJournalEntryLinux() {
+	content, success := writeEntryInNeovim()
+	if !success {
+		fmt.Println("Failed to write journal entry in Neovim.")
+		return
+	}
+
+	fmt.Println("Journal entry content:")
+	fmt.Println(string(content))
+}
+
+func writeEntryInNeovim() ([]byte, bool) {
+	fmt.Println("Opening Neovim editor.")
+
+	cmd := exec.Command("nvim")
+	cmd.Dir = config.ViperConfig.GetString("obsidian.vaultpath")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Create a temporary file
+	tmpFile, err := os.CreateTemp("", "journal_*.md")
+	if err != nil {
+		fmt.Println("Error creating temporary file:", err)
+		return nil, false
+	}
+	defer os.Remove(tmpFile.Name())             // Clean up the temporary file after use
+	cmd.Args = append(cmd.Args, tmpFile.Name()) // Pass the temporary file to Neovim
+	fmt.Println("Temporary file created:", tmpFile.Name())
+
+	// Open Neovim with the temporary file
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("Error starting Neovim:", err)
+		return nil, false
+	}
+	fmt.Println("Neovim editor opened. Please write your journal entry.")
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println("Error waiting for Neovim to close:", err)
+		return nil, false
+	}
+
+	// Read the content of the temporary file
+	content, err := os.ReadFile(tmpFile.Name())
+	if err != nil {
+		fmt.Println("Error reading temporary file:", err)
+		return nil, false
+	}
+	return content, true
 }
