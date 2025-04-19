dd1dd8ec22ea8e3da6a98b4093385585410b41fd
matkv <mail@matkv.dev>
2025-04-19 16:52:26 +0200

    use const strings instead of hardcoded values for reading config

--- internal/journal/obsidian_journal.go
+++ internal/journal/obsidian_journal.go
@@ -8,9 +8,11 @@ import (
 	"github.com/matkv/utils/internal/config"
 )
 
-// Idea: Just run "utils journal" - this opens up neovim if on linux, otherwise windows editor
-// creates temporary file somewhere, opens it, and then deletes it when closed.
-// once file is closed, the content of it will be appended to the journal file.
+const (
+	ConfigTypeKey     = "configType"
+	ConfigTypeLinux   = "linux"
+	ConfigTypeWindows = "windows"
+)
 
 type Journal struct {
 	ObsidianPath string
@@ -26,18 +28,23 @@ func CreateJournalEntry() {
 	openEditor()
 }
 
+func getOperatingSystem() string {
+	operatingSystem := config.ViperConfig.GetString(ConfigTypeKey)
+	return operatingSystem
+}
+
 func openEditor() {
-	operatingSystem := config.ViperConfig.GetString("configType")
-	fmt.Println("Operating system:", operatingSystem)
+	operatingSystem := getOperatingSystem()
+	if operatingSystem == "" {
+		fmt.Println("No operating system specified. Cannot open editor.")
+		return
+	}
 
-	if operatingSystem == "linux" {
+	if operatingSystem == ConfigTypeLinux {
 		createJournalEntryLinux()
-	}
-	if operatingSystem == "windows" {
+	} else if operatingSystem == ConfigTypeWindows {
 		fmt.Println("Opening Windows editor.")
-		// Open Windows editor here
 	}
-
 }
 
 func createJournalEntryLinux() {
