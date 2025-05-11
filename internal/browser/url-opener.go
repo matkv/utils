3e901638c0fdd6ee396c20df233e614e64768aac
matkv <mail@matkv.dev>
2025-05-11 21:16:40 +0200

    append https hardcoded to URLs for now

--- internal/browser/url-opener.go
+++ internal/browser/url-opener.go
@@ -14,6 +14,18 @@ func OpenURLS(urls []string) {
 	}
 	// Iterate over the provided URLs and open each one
 	for _, url := range urls {
+
+		// Check if the URL is valid
+		if len(url) == 0 {
+			fmt.Println("Empty URL provided. Skipping...")
+			continue
+		}
+
+		// append https:// if the URL doesn't start with http:// or https://
+		if !(len(url) >= 7 && (url[:7] == "http://" || url[:8] == "https://")) {
+			url = "https://" + url
+		}
+
 		if err := openURL(url); err != nil {
 			fmt.Printf("Error opening URL %s: %v\n", url, err)
 		} else {
@@ -23,7 +35,6 @@ func OpenURLS(urls []string) {
 }
 
 func openURL(url string) error {
-	browser.OpenURL(url)
 	if err := browser.OpenURL(url); err != nil {
 		return fmt.Errorf("failed to open URL %s: %w", url, err)
 	}
