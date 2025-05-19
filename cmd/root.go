ff384e695c45f9b6e6dd62d4663ee7313fca3106
matkv <mail@matkv.dev>
2025-05-19 20:31:32 +0200

    add flag for running in tray mode

--- cmd/root.go
+++ cmd/root.go
@@ -9,10 +9,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var IsTrayMode bool
+
 var rootCmd = &cobra.Command{
 	Use:   "utils",
 	Short: "CLI tool to automate some personal tasks",
 	Long:  `CLI tool to automate some personal tasks. Some of the tasks include managing dotfiles, managing my hugo website and updating my workout tracker.`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if IsTrayMode {
+			fmt.Println("Running in tray mode")
+		} else {
+			fmt.Println("Not running in tray mode")
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
@@ -20,6 +29,8 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.Flags().BoolVarP(&IsTrayMode, "tray", "t", false, "Run in tray mode")
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
