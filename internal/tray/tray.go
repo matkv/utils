36089d1ae3b45b046807e8626b3a0dfb29ec165a
matkv <mail@matkv.dev>
2025-05-19 21:21:19 +0200

    add utils browser call to tray

--- internal/tray/tray.go
+++ internal/tray/tray.go
@@ -5,6 +5,7 @@ import (
 
 	"fyne.io/systray"
 	"fyne.io/systray/example/icon"
+	"github.com/matkv/utils/internal/browser"
 )
 
 func SetupTrayMode() {
@@ -16,15 +17,15 @@ func onReady() {
 	systray.SetIcon((icon.Data))
 	systray.SetTitle("Utils")
 	systray.SetTooltip("CLI tool to automate some personal tasks")
-	mSayHello := systray.AddMenuItem("Say Hello", "Prints a greeting")
+	mBrowser := systray.AddMenuItem("Browser", "Run utils browser")
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 	mQuit.SetIcon(icon.Data)
 
 	go func() {
 		for {
 			select {
-			case <-mSayHello.ClickedCh:
-				fmt.Println("Hello!")
+			case <-mBrowser.ClickedCh:
+				browser.OpenURLS([]string{})
 			case <-mQuit.ClickedCh:
 				systray.Quit()
 				return
