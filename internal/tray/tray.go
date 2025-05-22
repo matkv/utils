d4e3ca71e17878a54b86e35a0f0bf5f37e37cc51
matkv <mail@matkv.dev>
2025-05-22 23:16:33 +0200

    add tray commands to start and quit komorebi

--- internal/tray/tray.go
+++ internal/tray/tray.go
@@ -2,6 +2,8 @@ package tray
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"fyne.io/systray"
 	"fyne.io/systray/example/icon"
@@ -18,6 +20,8 @@ func onReady() {
 	systray.SetTitle("Utils")
 	systray.SetTooltip("CLI tool to automate some personal tasks")
 	mBrowser := systray.AddMenuItem("Browser", "Run utils browser")
+	mKomorebiStart := systray.AddMenuItem("Komorebi Start", "Start komorebi & bar")
+	mKomorebiQuit := systray.AddMenuItem("Komorebi Quit", "Quit komorebi & bar")
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 	mQuit.SetIcon(icon.Data)
 
@@ -26,12 +30,40 @@ func onReady() {
 			select {
 			case <-mBrowser.ClickedCh:
 				browser.OpenURLS([]string{})
+			case <-mKomorebiStart.ClickedCh:
+				startKomorebi()
+			case <-mKomorebiQuit.ClickedCh:
+				quitKomorebi()
 			case <-mQuit.ClickedCh:
 				systray.Quit()
 				return
 			}
 		}
 	}()
+}
+
+func startKomorebi() {
+	cmd := exec.Command("komorebic", "start", "--bar", "--whkd")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting komorebi:", err)
+		return
+	}
+}
+
+func quitKomorebi() {
+	cmd := exec.Command("komorebic", "stop", "--bar", "--whkd")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting komorebi:", err)
+		return
+	}
 }
 
 func onExit() {
